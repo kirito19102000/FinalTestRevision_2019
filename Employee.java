fe0d6194140df1667eaef1aa1c3efee30fda4d98
NhutNguyen <43294263+NhutNguyen236@users.noreply.github.com>
2019-05-13 12:58:33 +0700

    Update Employee.java
    no need package deleted

--- Employee.java
+++ Employee.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package final_review;
-
-/**
- *
- * @author Admin
- */
 public abstract class Employee {
     protected String name;
     protected String DOB;
