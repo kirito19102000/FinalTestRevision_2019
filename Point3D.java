de9225b7f86f9be340f5d17e781f7b1e8bd0c96f
NhutNguyen <43294263+NhutNguyen236@users.noreply.github.com>
2019-05-13 16:37:48 +0700

    Add files via upload

---
+++ Point3D.java
@@ -0,0 +1,34 @@
+public class Point3D extends Point2D {
+    private float z;
+
+    public Point3D() {
+        this.z = 0.0f;
+    }
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public void setXYZ(float x, float y, float z) {
+        super.setXY(x,y);
+        setZ(z);
+    }
+
+    public float[] getXYZ() {
+        return new float[]{getX(), getY(), getZ()};
+    }
+
+    public String toString() {
+        return "(" + getXYZ()[0] + "," + getXYZ()[1] + "," + getXYZ()[2] + ")";
+    }
+
+}
