412c1d9934038c866b2372422d39cf571b19e2f8
NhutNguyen <43294263+NhutNguyen236@users.noreply.github.com>
2019-05-13 12:57:52 +0700

    Update Cylinder.java

--- Cylinder.java
+++ Cylinder.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package final_review;
-
-/**
- *
- * @author Admin
- */
 public class Cylinder extends Circle implements Shape{
     private double height;
     //This is Parameterized 
