eb0bd93d8b063df1432ea0de2f25e71c5ca8bdb0
NhutNguyen <43294263+NhutNguyen236@users.noreply.github.com>
2019-05-13 13:02:42 +0700

    Update Salesman.java

--- Salesman.java
+++ Salesman.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package final_review;
-
-/**
- *
- * @author Admin
- */
 public class Salesman extends Employee{
     private int productQuantity;
     //This is Parameterized
