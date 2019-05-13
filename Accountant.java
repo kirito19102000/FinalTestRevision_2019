0addb345508fb1c507a4c1ed4af92d7b896ed468
NhutNguyen <43294263+NhutNguyen236@users.noreply.github.com>
2019-05-13 13:02:14 +0700

    Update Accountant.java

--- Accountant.java
+++ Accountant.java
@@ -1,19 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package final_review;
-
-/**
- *
- * @author Admin
- */
 public class Accountant extends Employee{
     private double rate;
     //This is parameterized 
     public void Accountant(double rate){
         this.rate = rate;
+    }
+    //This is Non-parameterized
+    public void Accountant(){
+        this.rate = 0;
     }
     @Override
     public double getSalary() {
